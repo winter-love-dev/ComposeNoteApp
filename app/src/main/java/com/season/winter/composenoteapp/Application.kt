d8a2db0558d3a5319876720979e6ef0177fb1b41
hun73877 <hun230402@gmail.com>
2023-08-23 23:42:16 +0900

    set default dependencies with version catalog

---
+++ app/src/main/java/com/season/winter/composenoteapp/Application.kt
@@ -0,0 +1,8 @@
+package com.season.winter.composenoteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+}
--- app/src/main/java/com/season/winter/composenoteapp/activity/main/MainActivity.kt
+++ app/src/main/java/com/season/winter/composenoteapp/activity/main/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.season.winter.composenoteapp
+package com.season.winter.composenoteapp.activity.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,8 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.season.winter.composenoteapp.compose.ui.theme.ComposeNoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
